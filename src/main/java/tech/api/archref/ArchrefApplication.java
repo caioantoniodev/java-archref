a0dff02d17ae20445c373a0e5f937420ae96a175
Caio Antonio <caio.antonio.c@outlook.com>
2023-07-22 13:44:01 -0300

    project init

---
+++ src/main/java/tech/api/archref/ArchrefApplication.java
@@ -0,0 +1,13 @@
+package tech.api.archref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchrefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchrefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/api/archref/ArchrefApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.api.archref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchrefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
