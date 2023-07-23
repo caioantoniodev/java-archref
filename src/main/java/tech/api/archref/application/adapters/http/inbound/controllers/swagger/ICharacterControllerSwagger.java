aeefc98df7a4599c68e4f650931ea50e111352a0
Caio Antonio <caio.antonio.c@outlook.com>
2023-07-23 17:46:08 -0300

    wip

---
+++ src/main/java/tech/api/archref/application/adapters/http/inbound/controllers/swagger/ICharacterControllerSwagger.java
@@ -0,0 +1,18 @@
+package tech.api.archref.application.adapters.http.inbound.controllers.swagger;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tech.api.archref.application.adapters.http.inbound.controllers.dto.request.CharacterCreateRequest;
+
+@RequestMapping(value = "/v1/character", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Tag(name = "Character", description = "Operações referentes ao personagem")
+public interface ICharacterControllerSwagger {
+
+    @PostMapping
+    ResponseEntity<?> post(@RequestBody @Valid CharacterCreateRequest characterCreateRequest);
+}
